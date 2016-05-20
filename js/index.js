87e1d9d2c9faec0e15957af001f5e4f6566d231b
Paweł <paweltar@gmail.com>
2016-05-20 09:29:25 +0200

    Fix navbar bug

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	$('.hamburger').click(function() {
-		$('.nav-links').animate({ opacity: 'toggle' });
-    $('#home').toggleClass("add-padding-bottom");
+		$('.nav-links').toggleClass("visibility");
+		$('#home').toggleClass("add-padding-bottom");
+
+		if($(window).width() < 800) {
+			$('.nav-links').addClass("mobile-style");
+		} else {
+			$('.nav-links').removeClass("mobile-style");
+		}
 	});
 });
